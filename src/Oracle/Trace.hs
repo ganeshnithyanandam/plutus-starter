24095ec429847b2cb4e000ee9e6bf3db6c57363a
gannith <->
2022-03-20 23:23:19 +0530

    Mints marker NFT at Oracle

--- src/Oracle/Trace.hs
+++ src/Oracle/Trace.hs
@@ -35,30 +35,3 @@ oracleTrace :: EmulatorTrace ()
 oracleTrace = do
     h <- activateContractWallet alice startEndpoint
     callEndpoint @"start" h ()
-   {-void $ Emulator.waitNSlots 5
-    Last m <- observableState h
-    case m of
-        Nothing -> Extras.logError @String "error starting token sale"
-        Just ts -> do
-            Extras.logInfo $ "started token sale " ++ show ts
-            return ()-}
-
-
-            {-h1 <- activateContractWallet w1 $ useEndpoints ts
-            h2 <- activateContractWallet w2 $ useEndpoints ts
-            h3 <- activateContractWallet w3 $ useEndpoints ts
-
-            callEndpoint @"set price" h1 1_000_000
-            void $ Emulator.waitNSlots 5
-
-            callEndpoint @"add tokens" h1 100
-            void $ Emulator.waitNSlots 5
-
-            callEndpoint @"buy tokens" h2 20
-            void $ Emulator.waitNSlots 5
-
-            callEndpoint @"buy tokens" h3 5
-            void $ Emulator.waitNSlots 5
-
-            callEndpoint @"close" h1 ()
-            void $ Emulator.waitNSlots 5-}
